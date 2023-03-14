aac7f9523154913cf352625bb80be5a2dff50e8d
SShingler <s.shingler@hotmail.co.uk>
2023-03-14 16:47:32 +0000

    update notes / add story author&score

--- src/Components/Story.js
+++ src/Components/Story.js
@@ -7,6 +7,12 @@ const Story = ({details}) => {
         <li>
             <h1><a href={details.url} target="_blank">{details.title} </a></h1>
         </li>
+        <li>
+            <h3>Posted by: '{details.by}'</h3> 
+        </li>
+        <li>
+        <h3> Score:{details.score}</h3>
+        </li>
         </>
     );
     };
--- src/Containers/StoryContainer.js
+++ src/Containers/StoryContainer.js
@@ -8,28 +8,35 @@ const StoryContainer = () => {
     const [filterStories, setFilteredStories] = useState([]);
 
     useEffect(() => {
-        fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
-        .then(res => res.json())
-        .then((data) => {
-            const copyStoryArray = [...data];
-            const reducedStoryArray = copyStoryArray.splice(0, 25);
-            //console.log(reducedStoryArray);
-    
+        fetch('https://hacker-news.firebaseio.com/v0/topstories.json')  //call api 
+        .then(res => res.json())     //get results, turn into results.json
+        .then((data) => {              //results.json = 'data'
+            const copyStoryArray = [...data];     //make a copy of 'data' array into 'CopyStoryArray'
+            const reducedStoryArray = copyStoryArray.splice(0, 25);         //reduce/splice 'CopyStoryAway' into 'reducedStoryArray' - index position 0 to 25.
+            //console.log(reducedStoryArray)            //able to view in browser tools 'console' 
+            
+
+
+            //create 'promises' - copy 'id' within 'reducedStoryArray'.
             const promises = reducedStoryArray.map((id) => {
-                return fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+                return fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)    //loop the new array of 'promise' id's - injecting them into URL 
                 .then((res) => res.json());
             });
-    
+            
+            //provide data in 'promises' to 'Promise.All' 
+            //update 'setStories' 
+
             Promise.all(promises).then((data) => {
                 setStories(data)
             });
         });
     }, []); 
 
+    //pass filter function down as props 
     const filter = (storySearch) => {
-        const makeLowerCase= storySearch.toLowerCase();
+        const makeLowerCase= storySearch.toLowerCase();             //turn searchterm into lowercase    
         const filterStories = stories.filter((story) => {
-            return story.title.toLowerCase().includes(makeLowerCase);
+            return story.title.toLowerCase().includes(makeLowerCase); 
         });
         
         setFilteredStories(filterStories);
@@ -40,10 +47,11 @@ const StoryContainer = () => {
   
 
     return (
+        //pass filter function down as props 
         <>
         <h1>Top Stories</h1>
-        <Filter handleChange={filter}/>
-        <StoryList stories={filterStories}/>
+        <Filter handleChange={filter}/>  
+         <StoryList stories={filterStories}/>
         </>
     );
 
